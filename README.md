df2cbd7f1a67f2b5d85abba2a67b1f04e5183b52
Rostislav <rostwik@gmail.com>
2023-01-27 02:06:11 +0300

    Refactoring

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ $pip install -r requirements.txt
 ```
 - run the program
 ```bash
-$python main.py"
+$python main.py
 ```
 
 ## License
--- main.py
+++ main.py
@@ -1,40 +1,29 @@
 import os
+import time
+import textwrap
 
 import requests
 import telegram
 from dotenv import load_dotenv
 
 
-def get_homeworks_list(devman_token, check_result):
-    headers = {
-        'Authorization': f'Token {devman_token}'
-    }
-    payload = {}
-
-    if check_result:
-        payload['timestamp'] = check_result['timestamp']
-
-    url = 'https://dvmn.org/api/long_polling/'
-
-    response = requests.get(url, headers=headers, params=payload, timeout=60)
-    response.raise_for_status()
-    check_result = response.json()['new_attempts'][0]
-
-    return check_result
-
-
 def send_telegram_msg(chat_id, check_result, telegram_token):
     bot = telegram.Bot(token=telegram_token)
 
     if check_result['is_negative']:
-        text = f'У Вас проверили работу "{check_result["lesson_title"]}".' \
-               f' К сожалению, в работе нашли ошибки.' \
-               f' Ссылка на урок: {check_result["lesson_url"]}'
-    else:
-        text = f'У Вас проверили работу "{check_result["lesson_title"]}".' \
-               f' Преподавателю все понравилось, можно приступать к следующему уроку.'
+        text = f'''
+                   У Вас проверили работу "{check_result['lesson_title']}".
+                   К сожалению, в работе нашли ошибки.
+                   Ссылка на урок: {check_result['lesson_url']}
+                   '''
 
-    bot.send_message(chat_id=chat_id, text=text)
+    else:
+        text = f'''
+                   У Вас проверили работу "{check_result['lesson_title']}".
+                   Преподавателю все понравилось, можно приступать к следующему уроку.
+                   '''
+
+    bot.send_message(chat_id=chat_id, text=textwrap.dedent(text))
 
 
 def main():
@@ -47,12 +36,27 @@ def main():
 
     while True:
         try:
-            check_result = get_homeworks_list(devman_api_token, check_result)
+            headers = {
+                'Authorization': f'Token {devman_api_token}'
+            }
+            payload = {}
+
+            if check_result:
+                payload['timestamp'] = check_result['timestamp']
+
+            url = 'https://dvmn.org/api/long_polling/'
+
+            response = requests.get(url, headers=headers, params=payload, timeout=60)
+            response.raise_for_status()
+            check_result = response.json()['new_attempts'][0]
+
             send_telegram_msg(telegram_chat_id, check_result, telegram_api_token)
-        except requests.exceptions.ReadTimeout:
+
+        except requests.exceptions.Timeout:
             print('Новых работ нет')
         except requests.exceptions.ConnectionError:
             print('Интернет исчез')
+            time.sleep(5)
 
 
 if __name__ == '__main__':
