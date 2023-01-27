819604431a9fe1a147543b6d302a8e78a7a08696
Rostislav <rostwik@gmail.com>
2023-01-27 17:39:23 +0300

    Refactoring

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ import telegram
 from dotenv import load_dotenv
 
 
-def send_telegram_msg(chat_id, check_result, telegram_token):
+def send_telegram_msg(chat_id, results, telegram_token):
     bot = telegram.Bot(token=telegram_token)
 
-    if check_result['is_negative']:
+    if results['is_negative']:
         text = f'''
-                   У Вас проверили работу "{check_result['lesson_title']}".
+                   У Вас проверили работу "{results['lesson_title']}".
                    К сожалению, в работе нашли ошибки.
-                   Ссылка на урок: {check_result['lesson_url']}
+                   Ссылка на урок: {results['lesson_url']}
                    '''
 
     else:
         text = f'''
-                   У Вас проверили работу "{check_result['lesson_title']}".
+                   У Вас проверили работу "{results['lesson_title']}".
                    Преподавателю все понравилось, можно приступать к следующему уроку.
                    '''
 
@@ -32,7 +32,7 @@ def main():
     devman_api_token = os.getenv('DEVMAN_API_TOKEN')
     telegram_api_token = os.getenv('TELEGRAM_API_TOKEN')
     telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-    check_result = {}
+    results = {}
 
     while True:
         try:
@@ -41,19 +41,23 @@ def main():
             }
             payload = {}
 
-            if check_result:
-                payload['timestamp'] = check_result['timestamp']
+            if results:
+                if results['status'] == 'found':
+                    payload['timestamp'] = results['last_attempt_timestamp']
+                if results['status'] == 'timeout':
+                    payload['timeout'] = results['timestamp_to_request']
 
             url = 'https://dvmn.org/api/long_polling/'
 
             response = requests.get(url, headers=headers, params=payload, timeout=60)
             response.raise_for_status()
-            check_result = response.json()['new_attempts'][0]
+            results = response.json()
 
-            send_telegram_msg(telegram_chat_id, check_result, telegram_api_token)
+            for result in results['new_attempts']:
+                send_telegram_msg(telegram_chat_id, result, telegram_api_token)
 
-        except requests.exceptions.Timeout:
-            print('Новых работ нет')
+        except requests.exceptions.ReadTimeout:
+            pass
         except requests.exceptions.ConnectionError:
             print('Интернет исчез')
             time.sleep(5)
